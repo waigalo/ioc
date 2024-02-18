2055c378025b6a5c5c6acf8bb75af7600e5cd637
WAIGALO <waigalosory913@gmail.com>
2024-02-18 11:58:16 +0000

    pres V2

--- src/main/java/com/vvdev/ioc/enset/IocEnset.java
+++ src/main/java/com/vvdev/ioc/enset/IocEnset.java
@@ -10,7 +10,7 @@ package com.vvdev.ioc.enset;
  */
 public class IocEnset {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+    //public static void main(String[] args) {
+    //    System.out.println("Hello World!");
+    //}
 }
